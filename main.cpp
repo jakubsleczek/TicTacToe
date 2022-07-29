d8617d55a17e3c2253f86505967acd300263b6c9
jakubsleczek <66244033+jakubsleczek@users.noreply.github.com>
2022-07-29 11:50:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 #include <cstdlib>
+#include <array>
 
-void display(const char *boxes);
+void display(const std::array<char, 9> &boxes);
+void refresh();
 
 int main(){
-char boxes[9] {'1','2','3','4','5','6','7','8','9'};
+std::array<char, 9> boxes {'1','2','3','4','5','6','7','8','9'};
 bool playerNumber{false};
 bool nextRound{true};
 int number;
 int movesCounter = 0;
 do{
-system("clear");
+refresh();
 display(boxes);
-std::cout<<"\n\nPlayer "<<static_cast<int>(playerNumber)+1<<" enters a number:";
+std::cout<<"\n\nPlayer "<<static_cast<int>(playerNumber)+1<<" enters a number: ";
 std::cin>>number;
 if(number!=1 && number!=2 && number!=3 && number!=4 && number!=5 && number!=6 && number!=7 && number!=8 && number!=9){
-    system("clear");
+    refresh();
     std::cout<<"*********Invalid command. Please try again**********\n\n";
     }
-else if (boxes[number-1]=='o' || boxes[number-1]=='x'){
-    system("clear");
+else if (boxes.at(number-1)=='o' || boxes.at(number-1)=='x'){
+    refresh();
     std::cout<<"*********Already taken! Try again*********\n\n";
     }
 else{
     movesCounter++;
     if(playerNumber)
-        boxes[number-1]='o';
+        boxes.at(number-1)='o';
     else
-        boxes[number-1]='x';
+        boxes.at(number-1)='x';
 
-    if((boxes[0]==boxes[1]&&boxes[0]==boxes[2]) || (boxes[3]==boxes[4]&&boxes[3]==boxes[5]) ||
-        (boxes[6]==boxes[7]&& boxes[6]==boxes[8]) || (boxes[0]==boxes[3]&&boxes[0]==boxes[6]) ||
-        (boxes[1]==boxes[4]&&boxes[1]==boxes[7]) || (boxes[2]==boxes[5]&&boxes[2]==boxes[8]) ||
-        (boxes[0]==boxes[4]&&boxes[0]==boxes[8]) || (boxes[2]==boxes[4]&&boxes[2]==boxes[6])){
-        system("clear");
+    if((boxes.at(0)==boxes.at(1)&&boxes.at(0)==boxes.at(2)) || (boxes.at(3)==boxes.at(4)&&boxes.at(3)==boxes.at(5)) ||
+        (boxes.at(6)==boxes.at(7)&& boxes.at(6)==boxes.at(8)) || (boxes.at(0)==boxes.at(3)&&boxes.at(0)==boxes.at(6)) ||
+        (boxes.at(1)==boxes.at(4)&&boxes.at(1)==boxes.at(7)) || (boxes.at(2)==boxes.at(5)&&boxes.at(2)==boxes[8]) ||
+        (boxes.at(0)==boxes.at(4)&&boxes.at(0)==boxes.at(8)) || (boxes.at(2)==boxes.at(4)&&boxes.at(2)==boxes.at(6))){
+        refresh();
         std::cout<<"*********Player "<<static_cast<int>(playerNumber)+1<<" wins!*********\n\n";
         nextRound = !nextRound;
         }
     playerNumber = !playerNumber;
 }
 if(movesCounter==9 && nextRound==true){
-system("clear");
+refresh();
 std::cout<<"*********It's a draw!*********\n\n";
 nextRound = !nextRound;
 }
@@ -50,18 +52,23 @@ display(boxes);
 return 0;
 }
 
-void display(const char *boxes){
+void display(const std::array<char, 9> &boxes){
 std::cout<<"\n\tTic Tac Toe\n\n";
 std::cout<<"Player 1 <"<<"x"<<">\tPlayer 2 <"<<"o"<<">\n\n";
 std::cout<<"     |     |     "<<std::endl;
-std::cout<<"  "<<boxes[0]<<"  |  "<<boxes[1]<<"  |  "<<boxes[2]<<"  "<<std::endl;
+std::cout<<"  "<<boxes.at(0)<<"  |  "<<boxes.at(1)<<"  |  "<<boxes.at(2)<<"  "<<std::endl;
 std::cout<<"     |     |     "<<std::endl;
 std::cout<<"-----|-----|-----"<<std::endl;
 std::cout<<"     |     |     "<<std::endl;
-std::cout<<"  "<<boxes[3]<<"  |  "<<boxes[4]<<"  |  "<<boxes[5]<<"  "<<std::endl;
+std::cout<<"  "<<boxes.at(3)<<"  |  "<<boxes.at(4)<<"  |  "<<boxes.at(5)<<"  "<<std::endl;
 std::cout<<"     |     |     "<<std::endl;
 std::cout<<"-----|-----|-----"<<std::endl;
 std::cout<<"     |     |     "<<std::endl;
-std::cout<<"  "<<boxes[6]<<"  |  "<<boxes[7]<<"  |  "<<boxes[8]<<"  "<<std::endl;
+std::cout<<"  "<<boxes.at(6)<<"  |  "<<boxes.at(7)<<"  |  "<<boxes.at(8)<<"  "<<std::endl;
 std::cout<<"     |     |     "<<std::endl;
+}
+
+void refresh(){
+//system("clear"); //linux
+system("cls"); //windows
 }
